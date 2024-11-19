b592327e438ef54af0647ca7b8930692ae7c449d
Abhinav-Rusia <abhinavrusia@gmail.com>
2024-11-19 11:52:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const sidebar = document.querySelector(".sidebar");
+
+function showSidebar() {
+  sidebar.style.display = "flex";
+}
+
+function closeSidebar() {
+    sidebar.style.display = "none";
+}
